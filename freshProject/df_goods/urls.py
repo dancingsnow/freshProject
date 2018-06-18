a57e35699fb8cf3d86c69b386e6cb7316741b8ee
snow <dancinginmysnow@gmail.com>
2018-06-18 20:13:05 +0800

    商品模块 -首页完成（未添加商品链接）

--- freshProject/df_goods/urls.py
+++ freshProject/df_goods/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 import views
 urlpatterns = [
     url(r'^$|^index/$',views.index),
-    # url(r'adv/(\d+)',views.advtising), # 广告位置
     url(r'^list/$',views.list),
-    url(r'^detail/$', views.detail),
-    url(r'^adv/(\d+)/$',views.adv_link),
+    url(r'^detail/(\d+)/$', views.detail),
+    url(r'^adv/(\d+)/$',views.adv_link),  # 广告位置
+
 ]
--- freshProject/df_goods/views.py
+++ freshProject/df_goods/views.py
@@ -6,11 +6,17 @@ from .models import *
 # index首页
 def index(request):
     adv = Advertising.objects.all()
-    fruits = GoodsInfo.objects.filter(isDelete=0,gtype_id=1)   # gtype_id = 2   ,过滤外键的方式
-    # print(fruits)
-    # print(type(fruits))
-    # print(len(fruits))
+    typelist = TypeInfo.objects.all()
+    # fruits = GoodsInfo.objects.filter(isDelete=0,gtype_id=1)
+    # gtype_id = 2   ,过滤外键的方式
+    # fru = typelist[0].goodsinfo_set.filter(id=1)  # 利用外键追溯到属于它的数据
+    # print(fru[0].gtitle)
+    fruits_word = typelist[0].goodsinfo_set.filter(isDelete=0).order_by('-gremain_num') #标题边的推荐文字内容
+    fruits = typelist[0].goodsinfo_set.filter(isDelete=0).order_by('-id')  # 水果默认推荐最新的
+    sea_word = typelist[1].goodsinfo_set.filter(isDelete=0).order_by('-gremain_num') # 海鲜水产
+    sea = typelist[1].goodsinfo_set.filter(isDelete=0).order_by('-id')
 
+# 还有猪牛羊肉、禽类蛋品、新鲜蔬菜、速冻食品等模块，同样的方式！
 
     context = {
         'title':'首页',
@@ -19,7 +25,10 @@ def index(request):
         'adv02_link':adv[1].alink,
         'adv01':adv[0].apic,
         'adv02':adv[1].apic,
-        'fruits':fruits,
+        'fruits_word':fruits_word[0:3],
+        'fruits':fruits[0:4],
+        'sea_word': sea_word[0:3],
+        'sea': sea[0:4],
     }
     return render(request,'df_goods/index.html',context)
 
@@ -39,6 +48,10 @@ def list(request):
     return render(request,'df_goods/list.html',context)
 
 # 商品细节界面
-def detail(request):
-    context = {'loadin':0}
+def detail(request,num_id):
+
+    context = {
+        'loadin':0,
+        'num_id':num_id,
+    }
     return render(request,'df_goods/detail.html',context)
